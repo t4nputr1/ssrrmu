09b187f623d93eef21e5aa8e2b1db6502d38f88b
t4nputr1 <78493181+t4nputr1@users.noreply.github.com>
2021-02-28 19:13:19 +0700

    Update anu.sh

--- anu.sh
+++ anu.sh
@@ -19,7 +19,7 @@ echo "Please enter the username you want to set (do not repeat, does not support
 read -e -p "(Default: ):" ssr_user
 echo && echo ${Separator_1} && echo -e "	username : ${ssr_user}
 read -p "Masa Aktif (hari): " masaaktif
-exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+exp= "date -d "$masaaktif days" +"%Y-%m-%d" "
 
 lastport=$(cat /usr/local/shadowsocksr/mudb.json | grep '"port": ' | tail -n1 | awk '{print $2}' | cut -d "," -f 1 | cut -d ":" -f 1 )
 ssr_port=$((lastport+1))
